a7b5f63c42a2752b8c45447ae40eaee648958499
Kirn Gill II <segin2005@gmail.com>
2025-07-06 14:21:31 -0500

    feat: Add watchdog timer to unit tests

--- dispatch_server_cpp/tests/main.cpp
+++ dispatch_server_cpp/tests/main.cpp
@@ -1,6 +1,39 @@
 #include <gtest/gtest.h>
+#include <thread>
+#include <chrono>
+#include <iostream>
+#include <condition_variable>
+#include <mutex>
+
+std::mutex m;
+std::condition_variable cv;
+bool tests_completed = false;
+
+void watchdog_timer(int timeout_seconds) {
+    std::unique_lock<std::mutex> lock(m);
+    if (cv.wait_for(lock, std::chrono::seconds(timeout_seconds), []{ return tests_completed; })) {
+        // Tests completed in time.
+    } else {
+        std::cerr << "!!! TEST TIMEOUT (" << timeout_seconds << "s) !!!" << std::endl;
+        std::exit(1);
+    }
+}
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+
+    const int timeout_seconds = 300; // 5 minutes
+    std::thread watchdog(watchdog_timer, timeout_seconds);
+
+    int result = RUN_ALL_TESTS();
+
+    {
+        std::lock_guard<std::mutex> lock(m);
+        tests_completed = true;
+    }
+    cv.notify_one();
+
+    watchdog.join();
+
+    return result;
 }
